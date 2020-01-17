b424a2e158717e34437aa6c9114ec1b9fb7f8b1e
Adam Dangoor <adamdangoor@gmail.com>
2020-01-17 23:46:13 +0000

    Add stubs for tests

--- tests/test_requests_mock_flask.py
+++ tests/test_requests_mock_flask.py
@@ -2,28 +2,19 @@
 Tests for the ``requests_mock_flask`` package.
 """
 
-from flask import Flask, jsonify
+from typing import Tuple
 
-def test_flask_client() -> None:
+from flask import Flask, Response, jsonify
+
+def test_simple_route() -> None:
     """
     Test with a trivial Flask application.
     """
     trivial_flask_app = Flask(__name__)
 
     @trivial_flask_app.route('/')
-    def hello_world():
+    def example() -> str:
         return 'Hello, World!'
-
-    @trivial_flask_app.route('/something_json')
-    def hello_world_json():
-        return jsonify({'hello': 'world'})
-
-    @trivial_flask_app.route('/something_json/<')
-    def hello_world_with_id():
-        return jsonify({'hello': 'world'})
-
-    # TODO something with each ID pattern
-    # TODO something with a different HTTP verb
 
     test_client = trivial_flask_app.test_client()
     result = test_client.get('/')
@@ -31,6 +22,54 @@ def test_flask_client() -> None:
     assert result.headers['Content-Type'] == 'text/html; charset=utf-8'
     assert response.text == 'Hello World!'
 
-    # TODO with requests_mock
-    # TODO with responses
-    # TODO separate tests for each endpoint type
+def test_route_with_json():
+    trivial_flask_app = Flask(__name__)
+
+    @trivial_flask_app.route('/something_json')
+    def example() -> Tuple[Response, int]:
+        return jsonify({'hello': 'world'})
+
+    @trivial_flask_app.route('/something_json/<int: example_id>')
+    def example(example_id: int) -> Tuple[Response, int]:
+        return jsonify({'hello': 'world'})
+
+    test_client = trivial_flask_app.test_client()
+    result = test_client.get('/')
+    assert result.status_code == 200
+    assert result.headers['Content-Type'] == 'application/json'
+    assert response.text == 'Hello World!'
+
+def test_route_with_string_variable():
+    pass
+
+def test_route_with_int_variable():
+    pass
+
+def test_route_with_float_variable():
+    pass
+
+def test_route_with_path_variable():
+    pass
+
+def test_route_with_uuid_variable():
+    pass
+
+def test_route_with_multiple_variables():
+    pass
+
+def test_post_verb():
+    pass
+
+def test_multiple_http_verbs():
+    pass
+
+def test_custom_converter():
+    pass
+
+
+# TODO link to https://flask.palletsprojects.com/en/1.1.x/quickstart/#variable-rules
+# TODO test with a custom converter
+
+# TODO with requests_mock
+# TODO with responses
+# TODO maybe a for loop for each converter in url_map.converters
