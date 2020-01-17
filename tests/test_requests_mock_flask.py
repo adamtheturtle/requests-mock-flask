6db5681af29cc5019caff1fd0a72862c6888ae46
Adam Dangoor <adamdangoor@gmail.com>
2020-01-17 18:22:23 +0000

    Progress towards tests

--- tests/test_requests_mock_flask.py
+++ tests/test_requests_mock_flask.py
@@ -2,7 +2,7 @@
 Tests for the ``requests_mock_flask`` package.
 """
 
-from flask import Flask
+from flask import Flask, jsonify
 
 def test_flask_client() -> None:
     """
@@ -14,7 +14,22 @@ def test_flask_client() -> None:
     def hello_world():
         return 'Hello, World!'
 
+    @trivial_flask_app.route('/something_json')
+    def hello_world_json():
+        return jsonify({'hello': 'world'})
+
+    @trivial_flask_app.route('/something_json/<')
+    def hello_world_with_id():
+        return jsonify({'hello': 'world'})
+
+    # TODO something with each ID pattern
+    # TODO something with a different HTTP verb
+
     test_client = trivial_flask_app.test_client()
     result = test_client.get('/')
     assert result.status_code == 200
     assert result.headers['Content-Type'] == 'text/html; charset=utf-8'
+    assert response.text == 'Hello World!'
+
+    # TODO with requests_mock
+    # TODO with responses
