c9a0c88f832cceca6edb0899537f9e04205dbe96
Adam Dangoor <adamdangoor@gmail.com>
2020-01-17 15:11:39 +0000

    Fix mypy

--- tests/test_requests_mock_flask.py
+++ tests/test_requests_mock_flask.py
@@ -3,7 +3,7 @@ Tests for the ``requests_mock_flask`` package.
 """
 
 
-def test_remove_me():
+def test_remove_me() -> None:
     """
     Temporary test so that ``pytest`` passes before we have tests.
     """
