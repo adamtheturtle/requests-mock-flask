691fe1c830431d45d87c673de99033fe81dca5d1
Adam Dangoor <adamdangoor@gmail.com>
2020-01-18 17:04:07 +0000

    Simplify adding callbacks

--- src/requests_mock_flask/__init__.py
+++ src/requests_mock_flask/__init__.py
@@ -23,23 +23,14 @@ def add_flask_app_to_mock(
     """
     callback = partial(_request_callback, flask_app=flask_app)
     for rule in flask_app.url_map.iter_rules():
-        # We assume here that everything is in the style:
-        # "{uri}/{method}/<{id}>" or "{uri}/{method}" or
-        # "{uri}/{method}/<{type}:{id}>" when this is not necessarily the case.
-        #
         # We replace everything inside angle brackets with a match for any
         # string of characters of length > 0.
         path_to_match = re.sub(pattern='<.+>', repl='.+', string=rule.rule)
         pattern = urljoin(base_url, path_to_match)
+        url = re.compile(pattern)
 
         for method in rule.methods:
-            mock_obj.add_callback(
-                # ``responses`` has methods named like the HTTP methods
-                # they represent, e.g. ``responses.GET``.
-                method=getattr(responses, method),
-                url=re.compile(pattern),
-                callback=callback,
-            )
+            mock_obj.add_callback(method=method, url=url, callback=callback)
 
 
 def _request_callback(
@@ -56,13 +47,10 @@ def _request_callback(
     :return: A tuple of status code, response headers and response data
         from the flask app.
     """
-    # The Flask test client is a ``werkzeug.test.Client`` and therefore has
-    # methods like 'head', 'get' and 'post'.
-    lower_request_method = str(request.method).lower()
     test_client = flask_app.test_client()
-    test_client_method = getattr(test_client, lower_request_method)
-    response = test_client_method(
+    response = test_client.open(
         path=request.path_url,
+        method=request.method,
         headers=dict(request.headers),
         data=request.body,
     )
