b81253028cff6cc7a0e96d58b6d690c9e2ee171e
Adam Dangoor <adamdangoor@gmail.com>
2020-01-18 12:08:44 +0000

    Progress towards tests

--- tests/test_requests_mock_flask.py
+++ tests/test_requests_mock_flask.py
@@ -17,29 +17,30 @@ def test_simple_route() -> None:
         return 'Hello, World!'
 
     test_client = app.test_client()
-    result = test_client.get('/')
-    assert result.status_code == 200
-    assert result.headers['Content-Type'] == 'text/html; charset=utf-8'
-    assert response.text == 'Hello World!'
+    response = test_client.get('/')
+    assert response.status_code == 200
+    assert response.headers['Content-Type'] == 'text/html; charset=utf-8'
+    assert response.data == b'Hello, World!'
 
 def test_route_with_json():
     app = Flask(__name__)
 
-    @app.route('/something_json')
+    @app.route('/')
     def example() -> Tuple[Response, int]:
         return jsonify({'hello': 'world'})
 
     test_client = app.test_client()
-    result = test_client.get('/')
-    assert result.status_code == 200
-    assert result.headers['Content-Type'] == 'application/json'
-    assert response.text == 'Hello World!'
+    response = test_client.get('/')
+    assert response.status_code == 200
+    assert response.headers['Content-Type'] == 'application/json'
+    expected_data = {'hello': 'world'}
+    assert response.json == expected_data
 
 def test_route_with_variable_no_type_given():
 
     app = Flask(__name__)
 
-    @app.route('/something_json/<my_variable>')
+    @app.route('/<my_variable>')
     def example(my_variable: str) -> Tuple[Response, int]:
         return 'Hello: ' + my_variable
 
@@ -48,7 +49,7 @@ def test_route_with_string_variable():
 
     app = Flask(__name__)
 
-    @app.route('/something_json/<string: my_variable>')
+    @app.route('/<string:my_variable>')
     def example(my_variable: str) -> Tuple[Response, int]:
         return 'Hello: ' + my_variable
 
@@ -56,21 +57,30 @@ def test_route_with_string_variable():
 def test_route_with_int_variable():
     app = Flask(__name__)
 
-    @app.route('/something_json/<int: my_variable>')
+    @app.route('/<int:my_variable>')
     def example(my_variable: int) -> Tuple[Response, int]:
         return 'Hello: ' + str(my_variable + 5)
 
 def test_route_with_float_variable():
     app = Flask(__name__)
 
-    @app.route('/something_json/<int: my_variable>')
+    @app.route('/<int:my_variable>')
     def example(my_variable: float) -> Tuple[Response, int]:
         return 'Hello: ' + str(my_variable + 5)
 
 def test_route_with_path_variable_with_slash():
-    pass
+    app = Flask(__name__)
+
+    @app.route('/<path:my_variable>')
+    def example(my_variable: str) -> Tuple[Response, int]:
+        return 'Hello: ' + my_variable
 
 def test_route_with_uuid_variable():
+    # TODO
+    pass
+
+def test_nested_path():
+    # TODO variable after a non-variable, and then with another path after?
     pass
 
 def test_route_with_multiple_variables():
