ca9cb11e9d94f8698b6b3f54082c44fded8e948b
Adam Dangoor <adamdangoor@gmail.com>
2020-01-19 13:00:25 +0000

    Progress towards more tests

--- tests/test_requests_mock_flask.py
+++ tests/test_requests_mock_flask.py
@@ -709,6 +709,19 @@ def test_multiple_functions_same_path_different_type() -> None:
     assert response.headers['Content-Type'] == expected_content_type
     assert response.data == expected_data
 
+    with responses.RequestsMock(assert_all_requests_are_fired=False) as resp_m:
+        add_flask_app_to_mock(
+            mock_obj=resp_m,
+            flask_app=app,
+            base_url='http://www.example.com',
+        )
+
+        responses_response = requests.get('http://www.example.com/4')
+
+    assert responses_response.status_code == expected_status_code
+    assert responses_response.headers['Content-Type'] == expected_content_type
+    assert responses_response.text == expected_data.decode()
+
 
 def test_query_string() -> None:
     """
