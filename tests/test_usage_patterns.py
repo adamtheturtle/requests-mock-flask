cb9b388591c5a41f402334c8558251e30ddc63ab
Adam Dangoor <adamdangoor@gmail.com>
2020-01-20 08:50:15 +0000

    Initial stub test file

---
+++ tests/test_usage_patterns.py
@@ -0,0 +1,15 @@
+"""
+Tests for ways the helper can be used.
+"""
+
+
+class TestResponses:
+    """
+    Tests for using the helper with ``responses``.
+    """
+
+
+class TestRequestsMock:
+    """
+    Tests for using the helper with ``requests_mock``.
+    """
