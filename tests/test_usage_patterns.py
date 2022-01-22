6dc91d50587f082634517a346d01fe1fcb89131b
Adam Dangoor <adamdangoor@gmail.com>
2022-01-22 16:28:04 +0000

    Add a mypy ignore (needed until https://github.com/getsentry/responses/pull/469)

--- tests/test_usage_patterns.py
+++ tests/test_usage_patterns.py
@@ -40,7 +40,7 @@ class TestResponses:
         assert response.status_code == 200
         assert response.text == 'Hello, World!'
 
-    @responses.activate
+    @responses.activate  # type: ignore
     def test_decorator(self) -> None:
         """
         It is possible to use the helper with a ``responses`` decorator.
