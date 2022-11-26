6bdb7dcd2ade4772492c06fd66df412da9203a1f
Adam Dangoor <adamdangoor@gmail.com>
2022-11-26 13:46:35 +0000

    Switch to double quotes, allowing us to remove flake8-quotes, and therefore bump autoflake and flake8

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -11,24 +11,24 @@ from pkg_resources import get_distribution
 # pylint: disable=invalid-name
 
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.napoleon',
-    'sphinx_autodoc_typehints',
-    'sphinxcontrib.spelling',
-    'sphinx-prompt',
-    'sphinx_substitution_extensions',
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx_autodoc_typehints",
+    "sphinxcontrib.spelling",
+    "sphinx-prompt",
+    "sphinx_substitution_extensions",
 ]
 
-project = 'requests-mock-flask'
+project = "requests-mock-flask"
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # General information about the project.
-project = 'Requests-Mock-Flask'
+project = "Requests-Mock-Flask"
 year = datetime.datetime.now().year
-author = 'Adam Dangoor'
-project_copyright = f'{year}, {author}'
+author = "Adam Dangoor"
+project_copyright = f"{year}, {author}"
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -37,22 +37,22 @@ project_copyright = f'{year}, {author}'
 # Use ``pkg_resources`` as per
 # https://github.com/pypa/setuptools_scm#usage-from-sphinx.
 version = get_distribution(project).version
-_month, _day, _year, *_ = version.split('.')
-release = f'{_month}.{_day}.{_year}'
+_month, _day, _year, *_ = version.split(".")
+release = f"{_month}.{_day}.{_year}"
 
-html_theme = 'furo'
+html_theme = "furo"
 html_title = project
 html_show_copyright = False
 html_show_sphinx = False
 html_show_sourcelink = False
 html_theme_options = {
-    'sidebar_hide_name': False,
+    "sidebar_hide_name": False,
 }
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'requestsmockflaskdoc'
+htmlhelp_basename = "requestsmockflaskdoc"
 
-spelling_word_list_filename = '../../spelling_private_dict.txt'
+spelling_word_list_filename = "../../spelling_private_dict.txt"
 
 rst_prolog = f"""
 .. |project| replace:: {project}
@@ -73,4 +73,4 @@ set_type_checking_flag = True
 #   packages.
 # * Some packages cannot be imported when we use `typing.TYPE_CHECKING``, so
 #   we mock them out.
-autodoc_mock_imports = ['werkzeug', 'flask']
+autodoc_mock_imports = ["werkzeug", "flask"]
