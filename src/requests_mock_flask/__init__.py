9b9502b4bbf3e66b203090ff85cf3292c769c187
Adam Dangoor <adamdangoor@gmail.com>
2026-09-10 14:02:24 +0100

    Use a concrete protocol method body

--- src/requests_mock_flask/__init__.py
+++ src/requests_mock_flask/__init__.py
@@ -47,7 +47,7 @@ class _BinaryReader(Protocol):
 
     def read(self) -> bytes:
         """Read the remaining bytes."""
-        ...  # pylint: disable=unnecessary-ellipsis
+        raise NotImplementedError
 
 
 @dataclasses.dataclass(frozen=True)
